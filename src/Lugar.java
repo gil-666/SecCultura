0bb1ee6d3eb638f3a78ab49ea21a63a745f87475
Hugo Andrade <andradehugo470@gmail.com>
2023-08-29 22:33:16 -0500

    tabla gilberto

--- src/Lugar.java
+++ src/Lugar.java
@@ -1,13 +1,40 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+  
+import java.io.Serializable;
+import java.sql.PreparedStatement;
 
-/**
- *
- * @author andra
- */
-public class Lugar {
+public class Lugar implements Serializable {
+
+    String id_lugar,Nombre_lugar, Ubicacion, Capacidad, Tamano_m2;
+
+    public Lugar(String id_lugar, String Nombre_lugar, String Ubicacion, String Capacidad, String Tamano_m2) {
+        this.id_lugar = id_lugar;
+        this.Nombre_lugar = Nombre_lugar;
+        this.Ubicacion = Ubicacion;
+        this.Capacidad = Capacidad;
+        this.Tamano_m2 = Tamano_m2;
     
+    }
+    
+     public int insertar(Conexion cnx) {
+        try {
+            String sql = "INSERT INTO evento VALUES(?,?,?,?,?,?,?)";
+            PreparedStatement ps = cnx.con.prepareStatement(sql);
+            ps.setString(1, id_lugar);
+            ps.setString(2, Nombre_lugar);
+            ps.setString(3, Ubicacion);
+            ps.setString(4, Capacidad);
+            ps.setString(5, Tamano_m2);
+
+            
+            int resp = ps.executeUpdate();
+            return resp;
+            
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.getMessage());
+            return 0;
+        }
+    }
 }
+
+
+
