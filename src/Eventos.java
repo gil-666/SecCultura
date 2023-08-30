eac19ac96d2a5f4819036f60d9f8e18b93751058
sinthiaanaya <sinthiaanaya>
2023-08-30 00:50:11 -0500

    Merge origin/master
    Conflicts:
    	src/FEstudiantes.form
    	src/FEstudiantes.java

--- src/Eventos.java
+++ src/Eventos.java
@@ -1,4 +1,4 @@
-  
+
 import java.io.Serializable;
 import java.sql.Date;
 import java.sql.PreparedStatement;
@@ -6,9 +6,7 @@ import java.sql.Time;
 
 public class Eventos implements Serializable {
 
-    String Evento,  Tipo, Presupuesto, Fecha, Horario;
-    
-    
+    String Evento, Tipo, Presupuesto, Fecha, Horario;
 
     public Eventos(String Evento, String Tipo, String Presupuesto, String Fecha, String Horario) {
         this.Evento = Evento;
@@ -18,23 +16,19 @@ public class Eventos implements Serializable {
         this.Fecha = Fecha;
     }
 
-   
-
     public int insertar(Conexion cnx) {
         try {
             String sql = "INSERT INTO evento VALUES(?,?,?,?,?)";
             PreparedStatement ps = cnx.con.prepareStatement(sql);
             ps.setString(1, Evento);
-       
             ps.setString(2, Tipo);
             ps.setString(3, Presupuesto);
             ps.setString(4, Fecha);
             ps.setString(5, Horario);
 
-            
             int resp = ps.executeUpdate();
             return resp;
-            
+
         } catch (Exception e) {
             System.out.println("ERROR: " + e.getMessage());
             return 0;
